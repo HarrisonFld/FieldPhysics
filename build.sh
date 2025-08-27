a652fe5a6d90ccddce9b7a9a1bab8ca4c0bf7f1a
HarrisonFld <fieldharrison@proton.me>
2025-08-26 22:07:07 -0400

    Initialized Repository

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This whole script is kind of a pain to manage and defeats the purpose of cmake being multiplatform
+# TODO: find a better way to do this
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+BUILD_DIR="build/" # specify build directory
+BUILD_DIR=$SCRIPT_DIR/$BUILD_DIR # global build directory
+
+if [[ ! -d $BUILD_DIR ]]; then
+    mkdir -v $BUILD_DIR
+fi
+
+# Generate, Build, and Run the project
+cmake -S $SCRIPT_DIR -B $BUILD_DIR
+cmake --build $BUILD_DIR
+${BUILD_DIR}src/raylib-proj
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+
+int main() {
+    printf("Beginning Raylib Project");
+    return 0;
+}
