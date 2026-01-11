ece975da40f1ea5a9b1425380b3e68de8bbef769
HarrisonFld <fieldharrison@proton.me>
2026-01-11 15:39:42 -0500

    Made lib install

--- build.py
+++ build.py
@@ -11,6 +11,7 @@ if not os.path.isdir(BUILD_DIR):
     os.mkdir(BUILD_DIR)
 
 call_args = ""
+devBuild = True
 
 # Handle args
 for i in range(len(sys.argv)):
@@ -29,13 +30,20 @@ for i in range(len(sys.argv)):
                 sys.exit("Expected args after --call-args/-a")
             continue
 
+        case "--install" | "-i":
+            print("Running install, will require administrator permissions")
+            devBuild = False
+
 
 # Build and run project with cmake
 BUILD_CODE = -1
 CMAKE_CODE = os.system("cmake -S " + SCRIPT_DIR + " -B " + BUILD_DIR)
 if CMAKE_CODE == 0:
     BUILD_CODE = os.system("cmake --build " + BUILD_DIR)
-if BUILD_CODE == 0:
+if BUILD_CODE == 0 and devBuild:
     os.rename(BUILD_DIR + "/compile_commands.json", SCRIPT_DIR + "/compile_commands.json")
     print(BUILD_DIR + "/FieldEngineTest " + call_args)
     os.system(BUILD_DIR + "/FieldEngineTest " + call_args) # Run executable
+elif not devBuild:
+    print("Beginning install")
+    os.system("sudo cmake --install " + BUILD_DIR)
